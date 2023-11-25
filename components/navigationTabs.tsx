1394625ad2a79bd4ddeaba30ef15d425778a6184
ErikGbr3 <113256783+erikg-hash@users.noreply.github.com>
2023-11-25 14:14:06 -0600

    Suscripciones V2

--- components/navigationTabs.tsx
+++ components/navigationTabs.tsx
@@ -3,9 +3,7 @@ import LeaguesScreen from '../features/leagues/application/screens/leaguesScreen
 import MatchesScreen from '../features/matches/application/screens/matchesScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SportFieldsScreen from '../features/sportfields/application/screens/sporFieldsScreens';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import AddLeaguesScreen from '../features/leagues/application/screens/addLeaguesScreen';
-import PlayersScreen from '../features/players/application/screens/playersScreen';
 import AddPlayerScreen from '../features/players/application/screens/AddPlayersScreen';
 import SuscriptionScreen from '../features/suscriptions/application/screens/suscriptionsScreen';
 
@@ -15,9 +13,8 @@ const Tab = createNativeStackNavigator();
 export default function NavigationTabs() {
   return(
    <Tab.Navigator initialRouteName='Ligas' screenOptions={{headerShown: false }}>
-    <Tab.Screen name='Ligas' component={PlayersScreen}/>
-    <Tab.Screen name='Partidos' component={PlayersScreen}/>
-    <Tab.Screen name='agregarJugador' component={AddPlayerScreen}/>
+    <Tab.Screen name='Ligas' component={LeaguesScreen}/>
+    <Tab.Screen name='Partidos' component={MatchesScreen}/>
    </Tab.Navigator>
   )
 }
